d7ed26ee7aae291ae69dd27bf9d4c8588cc4a3c1
Hugo Mulana Christon <hugo.mulana@yahoo.com>
2023-07-13 20:06:45 +0700

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { fileURLToPath } from 'url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-calculator/',
   plugins: [react()],
   resolve: {
     alias: {
